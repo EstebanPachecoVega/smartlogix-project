47b7e783eb9a8cf36689420a1302c1f75580cf8f
Esteban Pacheco <es.pachecov@duocuc.cl>
2026-05-14 14:01:00 -0400

    se agrega DataInitializer y se actualiza ProductoMapper (no se estaba mapeando correctamente) y se actualiza application.yml por conflicto  al hacer build y ejecutar maven

--- backend/ms-inventario/src/main/java/cl/smartlogix/inventario/mapper/ProductoMapper.java
+++ backend/ms-inventario/src/main/java/cl/smartlogix/inventario/mapper/ProductoMapper.java
@@ -7,6 +7,8 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductoMapper {
+
     @Mapping(source = "id", target = "productoId")
+    @Mapping(source = "stock", target = "stockDisponible")
     StockResponseDTO toStockResponseDTO(Producto producto);
 }
