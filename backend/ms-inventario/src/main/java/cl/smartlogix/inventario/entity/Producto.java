488da6763e6642980c067274954686509e1e23fa
Esteban Pacheco <es.pachecov@duocuc.cl>
2026-05-14 12:12:38 -0400

    se agrega Producto en la carpeta entity del microservicio inventario

---
+++ backend/ms-inventario/src/main/java/cl/smartlogix/inventario/entity/Producto.java
@@ -0,0 +1,25 @@
+package cl.smartlogix.inventario.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "productos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String nombre;
+    
+    @Column(nullable = false)
+    private Integer stock;
+}
