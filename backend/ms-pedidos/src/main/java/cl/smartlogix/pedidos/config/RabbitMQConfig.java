098136f005436c143d3269fa718fd883cda2593f
Esteban Pacheco <es.pachecov@duocuc.cl>
2026-05-14 17:58:14 -0400

    fix: update ms-pedidos config and remove unused file

--- backend/ms-pedidos/src/main/java/cl/smartlogix/pedidos/config/RabbitMQConfig.java
+++ backend/ms-pedidos/src/main/java/cl/smartlogix/pedidos/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package cl.smartlogix.pedidos.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +27,10 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(enviosQueue())
                 .to(pedidoExchange())
                 .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
