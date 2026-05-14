4147a7b4d5cf6b6daab72894b30a30bfa71a0182
Esteban Pacheco <es.pachecov@duocuc.cl>
2026-05-14 17:56:14 -0400

    se actualiza rabbitmq config para que se consuman correctamente entre pedidos y envios y se elimina data initializer

--- backend/ms-envios/src/main/java/cl/smartlogix/envios/config/RabbitMQConfig.java
+++ backend/ms-envios/src/main/java/cl/smartlogix/envios/config/RabbitMQConfig.java
@@ -1,15 +1,14 @@
 package cl.smartlogix.envios.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String ENVIOS_QUEUE = "envios.queue";
-
     @Bean
-    public Queue enviosQueue() {
-        return new Queue(ENVIOS_QUEUE, true);
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
