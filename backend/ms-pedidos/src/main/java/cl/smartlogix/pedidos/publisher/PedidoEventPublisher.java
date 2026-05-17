b89f7af1051b4642ae9e531c4de1f2c4dbfb16d1
Esteban Pacheco <es.pachecov@duocuc.cl>
2026-05-17 01:42:16 -0400

    Se Actualiza microservio Pedidos con las nuevas configuraciones

--- backend/ms-pedidos/src/main/java/cl/smartlogix/pedidos/publisher/PedidoEventPublisher.java
+++ backend/ms-pedidos/src/main/java/cl/smartlogix/pedidos/publisher/PedidoEventPublisher.java
@@ -1,13 +1,15 @@
 package cl.smartlogix.pedidos.publisher;
 
 import cl.smartlogix.pedidos.dto.event.PedidoAprobadoEventDTO;
+import cl.smartlogix.pedidos.dto.event.PedidoRechazadoEventDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import static cl.smartlogix.pedidos.config.RabbitMQConfig.PEDIDO_EXCHANGE;
-import static cl.smartlogix.pedidos.config.RabbitMQConfig.ROUTING_KEY;
+import static cl.smartlogix.pedidos.config.RabbitMQConfig.ROUTING_KEY_APROBADO;
+import static cl.smartlogix.pedidos.config.RabbitMQConfig.ROUTING_KEY_RECHAZADO;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +18,12 @@ public class PedidoEventPublisher {
     private final RabbitTemplate rabbitTemplate;
 
     public void publicarPedidoAprobado(PedidoAprobadoEventDTO event) {
-        rabbitTemplate.convertAndSend(PEDIDO_EXCHANGE, ROUTING_KEY, event);
+        rabbitTemplate.convertAndSend(PEDIDO_EXCHANGE, ROUTING_KEY_APROBADO, event);
         log.info("Evento PedidoAprobado publicado: {}", event);
+    }
+
+    public void publicarPedidoRechazado(PedidoRechazadoEventDTO event) {
+        rabbitTemplate.convertAndSend(PEDIDO_EXCHANGE, ROUTING_KEY_RECHAZADO, event);
+        log.warn("⚠️ Evento PedidoRechazado (Compensación de Stock) enviado a RabbitMQ: {}", event);
     }
 }
