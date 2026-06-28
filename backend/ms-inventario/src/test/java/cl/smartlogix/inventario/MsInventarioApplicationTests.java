be292e2404f8f04249b008fdfdf4f1de4c32ddec
Esteban Pacheco <es.pachecov@duocuc.cl>
2026-06-28 12:29:06 -0400

    Mejorar los flujos de trabajo de CI y la configuración de pruebas para los servicios de backend.

--- backend/ms-inventario/src/test/java/cl/smartlogix/inventario/MsInventarioApplicationTests.java
+++ backend/ms-inventario/src/test/java/cl/smartlogix/inventario/MsInventarioApplicationTests.java
@@ -3,8 +3,10 @@ package cl.smartlogix.inventario;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Tag("docker")
 class MsInventarioApplicationTests {
 
